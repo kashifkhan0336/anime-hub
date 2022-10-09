044da2bde3f4343c6b5e42d3b851478d204573d9
Drago78624 <maazahmed78624@gmail.com>
2022-10-09 11:36:51 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import AnimeDetail from "./pages/AnimeDetail";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/anime/:id" element={<AnimeDetail />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
