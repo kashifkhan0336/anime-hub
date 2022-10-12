de52cfe9f59d12b291ab4d344e8f68502dc8cca9
Drago78624 <maazahmed78624@gmail.com>
2022-10-12 19:09:18 +0500

    pagination added but there is a bug

--- src/components/Animes.jsx
+++ src/components/Animes.jsx
@@ -2,14 +2,25 @@ import React from "react";
 import AnimeCard from "./AnimeCard";
 
 const Animes = ({ animeData }) => {
-    const displayAnimeCards = animeData.map(data => {
-        return <AnimeCard key={data._id} title={data.title} image={data.image} status={data.status} episodes={data.episodes} />
-    })
+  // const displayAnimeCards = currentItems && currentItems.map(data => {
+  //     return <AnimeCard key={data._id} title={data.title} image={data.image} status={data.status} episodes={data.episodes} />
+  // })
   return (
     <div className="animes">
       <h2 className="animes__heading heading">Showing Results</h2>
       <div className="animes__cards grid">
-        {displayAnimeCards}
+        {animeData &&
+          animeData.map((data) => {
+            return (
+              <AnimeCard
+                key={data._id}
+                title={data.title}
+                image={data.image}
+                status={data.status}
+                episodes={data.episodes}
+              />
+            );
+          })}
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ import Slide2 from "../assets/images/slide-2.jpg";
 import Slide3 from "../assets/images/slide-3.jpg";
 import Animes from "../components/Animes";
 import { options, fetchData, searchOptions } from "../fetchData";
-import ReactPaginate from 'react-paginate';
+import ReactPaginate from "react-paginate";
 
 const heroSliderImgs = [
   {
@@ -22,51 +22,56 @@ const heroSliderImgs = [
 ];
 
 const Home = () => {
-  const [animeData, setAnimeData] = useState([]);
-  const [currentItems, setCurrentItems] = useState(null);
+  const [animeData, setAnimeData] = useState([])
+  const [isLoaded, setisLoaded] = useState(false);
   const [pageCount, setPageCount] = useState(0);
-  const [itemOffset, setItemOffset] = useState(0); 
-  const itemsPerPage = 10
+  const [currentPage, setCurrentPage] = useState(1);
 
-  useEffect(() => {
-    const fetchAnimes = async () => {
-      const fetchedAnimes = await fetchData(
-        `https://anime-db.p.rapidapi.com/anime?page=1&size=50`,
-        searchOptions
-      );
-      console.log(fetchedAnimes)
-      setAnimeData(fetchedAnimes.data);
-    };
-    fetchAnimes()
-    const endOffset = itemOffset + itemsPerPage;
-    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-    setCurrentItems(animeData.slice(itemOffset, endOffset));
-    setPageCount(Math.ceil(animeData.length / itemsPerPage));
-  }, [itemOffset, itemsPerPage]);
-
-  const handlePageClick = (event) => {
-    const newOffset = (event.selected * itemsPerPage) % animeData.length;
-    console.log(
-      `User requested page number ${event.selected}, which is offset ${newOffset}`
-    );
-    setItemOffset(newOffset);
+  const handleFetch = async () => {
+    const animes = await fetchData(
+      `https://anime-db.p.rapidapi.com/anime?page=${currentPage}&size=10`,
+      searchOptions
+    )
+    console.log(animes)
+    setAnimeData([...animes.data]);
+    setPageCount(animes.meta.totalPage);
+    setisLoaded(true);
   };
+
+  useEffect(()=>{
+    handleFetch()
+  }, [])
+
+
+  const handlePageChange = (selectedObject) => {
+    console.log(selectedObject)
+		setCurrentPage(selectedObject.selected);
+		handleFetch();
+	};
 
   return (
     <div className="hero container">
       <Slider data={heroSliderImgs} perPage={1} />
-      <SearchAnime setAnimeData={setAnimeData} />
+      {/* <SearchAnime setAnimeData={setAnimeData} /> */}
       <Genre />
-      {currentItems ? <Animes animeData={currentItems} /> : "loading...." }
-      <ReactPaginate
-        breakLabel="..."
-        nextLabel="next >"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        pageCount={pageCount}
-        previousLabel="< previous"
-        renderOnZeroPageCount={null}
-      />
+      {isLoaded ? <Animes animeData={animeData} /> : "loading..."}
+      {isLoaded ? (
+				<ReactPaginate
+					pageCount={pageCount}
+					pageRange={1}
+					marginPagesDisplayed={1}
+					onPageChange={handlePageChange}
+					containerClassName={'paginate-container'}
+					previousLinkClassName={'page'}
+					breakClassName={'page'}
+					nextLinkClassName={'page'}
+					pageClassName={'page'}
+					disabledClassNae={'disabled'}
+					activeClassName={'active'}
+				/>
+			) : (
+				<div>Nothing to display</div>
+			)} 
     </div>
   );
 };
